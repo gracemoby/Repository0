53a6078e3ea5ecaf88937f20bd877529e1e2eba6
gracemoby <gracemoby.133@gmail.com>
2026-01-31 11:52:34 +0530

    Add files via upload

---
+++ leetcode.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static int[] plusOne(int[] digits) {
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {9, 9, 9};
+        int[] result = plusOne(digits);
+
+        for (int d : result) {
+            System.out.print(d);
+        }
+    }
+}
